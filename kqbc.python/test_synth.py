48a5617125fa4ae1eb51db4a3dbd345bb8240dec
Berry Weinstein <35626084+berryweinst@users.noreply.github.com>
2018-12-06 16:18:55 +0200

    Update test_synth.py

--- kqbc.python/test_synth.py
+++ kqbc.python/test_synth.py
@@ -18,10 +18,15 @@ X, y = get_linear_2d_data(5000)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
 
-
 svclassifier = SVC(kernel='linear')
 svclassifier.fit(X_train[:k,:], y_train[:k])
 y_pred = svclassifier.predict(X_test)
+np.sum(y_test==y_pred)/1000
 
 
 kqbc = KQBC(X_train, y_train, T=100, kernel='Linear', queries=k)
+
+svclassifier = SVC(kernel='linear')
+svclassifier.fit(X_train[kqbc,:], y_train[kqbc])
+y_pred = svclassifier.predict(X_test)
+np.sum(y_test==y_pred)/1000
