0a3b9ad8977dd07e5a55c263abe23f2ece5d71a9
Berry Weinstein <berryweinstein@Berrys-MacBook-Pro.local>
2019-03-01 19:07:46 +0200

    Added plots and working python kqbc

--- kqbc.py
+++ kqbc.py
@@ -1,8 +1,12 @@
 import numpy as np
 from scipy.linalg import schur
+from sklearn.metrics.pairwise import rbf_kernel
+import matlab.engine
+import copy
 
 
-def hit_n_run(x,A,T):
+
+def hit_n_run(x, A ,T):
     """
     Returns a random point using the hit and run algorithm from the
     convex body defined by Ax>=0 and ||x||<=1.
@@ -25,62 +29,46 @@ def hit_n_run(x,A,T):
     """
 
 
-    x = np.array([[478.2635],
-                    [464.3478],
-                      [770.7708],
-                       [54.7719]])
-
-
-    A = np.array([[-2.43819880e+00, 8.64835984e-01, -8.78624514e-02, 1.18932330e-01],
-               [-9.76110184e-01, -5.64887681e-01, -2.76434238e-01, -1.48243176e+00],
-               [1.68822239e+00, 6.94140818e-01, 8.81213677e-04, -1.80938297e+00]])
-
     dim = len(x)
     x = x[:]
-    u = np.random.randn(T,dim) # at step t the algorithm will pick a random point
+    u = np.random.randn(T, dim) # at step t the algorithm will pick a random point
                                # on the line through x and x+u(t,:)
     # u = np.arange(1, T * dim + 1).reshape(T, dim, order='F')
-    Au = np.matmul(u, A.T)
+    Au = u @ A.T
     nu = np.sum(u**2, 1)
-    l = np.random.randn(T, 1)
+    l = np.random.rand(T, 1)
     # l = np.arange(1, T + 1)
 
     for t in np.arange(0, T):
-        Ax = np.matmul(A, x)
-        ratio = -Ax / np.expand_dims(Au[t,:], 1)
-        I = np.where(Au[t,:] > 0)
+        Ax = A @ x
+        ratio = -np.divide(Ax, Au[t, :][np.newaxis].T)
+        I = np.where(Au[t,:] > 0)[0]
         mn = np.append(ratio[I], -np.inf).max()
-        I = np.where(Au[t,:] < 0)
+        I = np.where(Au[t, :] < 0)[0]
         mx = np.append(ratio[I], np.inf).min()
 
-        pre = np.matmul(nu[t], np.linalg.norm(x) ** 2 - 1) if nu[t].size > 1 else nu[t] * (np.linalg.norm(x) ** 2 - 1)
-        disc = np.matmul(x.T, u[t, :].T) ** 2 - pre
+        disc = ((x.T @ u[t, :].T) ** 2 - nu[t] * (np.linalg.norm(x) ** 2 - 1))[0]
         if disc < 0:
             print('negative disc, probably x is not a feasable point.')
             disc = 0
 
-        hl = (-np.matmul(x.T, u[t,:].T) + np.sqrt(disc)) / nu[t]
-        ll = (-np.matmul(x.T, u[t,:].T) - np.sqrt(disc)) / nu[t]
+        hl = (-(x.T @ u[t, :][np.newaxis].T) + np.sqrt(disc)) / nu[t]
+        ll = (-(x.T @ u[t, :][np.newaxis].T) - np.sqrt(disc)) / nu[t]
 
-        xx = min(hl, mx)
-        nn = max(ll, mn)
-        m = xx-nn
-        x0 = nn + np.matmul(l[t], m) if m.size > 1 else nn + l[t] * m
-        x1_pre = np.expand_dims(u[t,:], 1)
-        x1 = np.matmul(x1_pre, x0) if x0.size > 1 else x1_pre * x0
-        x = x + x1.reshape(x.shape)
-        print('step')
+        xx = min(hl[0], mx)
+        nn = max(ll[0], mn)
+        x = x + u[t, :][np.newaxis].T * (nn + l[t] * (xx-nn))
     return x
 
 
 
 
-def KQBC(X_train, Y_train, T, kernel, queries=100, **kwargs):
+def KQBC(K, Y_train, T, kernel, eng, queries=100, **kwargs):
     """
     Runs the kernelized version of the Query By Committee (QBC) algorithm.
 
     Inputs:
-        X_train - Training instances.
+        K       - Training instances with kernel applied.
         Y_train - Training labels (values +1 and -1).
         T       - Number of random walk steps to make when selecting a
                  random hypothesis.
@@ -99,20 +87,8 @@ def KQBC(X_train, Y_train, T, kernel, queries=100, **kwargs):
 
     tol = 1e-10 #tolerance for the factor function
 
-    if kernel == 'Linear':
-        K = np.matmul(X_train, X_train.T)
-    elif kernel == 'Poly':
-        K = (np.matmul(X_train, X_train.T) + kwargs['parameter1']) ** kwargs['parameter2']
-    elif kernel == 'Gauss':
-        nrm = np.sum(X_train ** 2, 1)
-        K = np.exp(-(np.tile(nrm, (1, len(Y_train))) - 2 * np.matmul(X_train, X_train.T) +
-                     np.tile(nrm.T, (len(Y_train), 1))) / (2 * kwargs['parameter1'] ** 2))
-    else:
-        print('Unknown kernel')
-
-    coefs = np.empty((Y_train.shape[0],0))
+    coefs = []
     errors = []
-    q_cnt = 0 # query counter
 
     samp_num = len(Y_train)
 
@@ -121,42 +97,40 @@ def KQBC(X_train, Y_train, T, kernel, queries=100, **kwargs):
 
     coef = np.zeros((len(Y_train), 1))
     coef[0] = Y_train[0] / np.sqrt(K[0, 0])
-    preds = np.matmul(K, coef)
-    errate = sum(np.multiply(Y_train.squeeze(), preds.squeeze()) <= 0)
+    # preds = K @ coef
+    # errate = sum(np.multiply(Y_train.squeeze(), preds.squeeze()) <= 0)
 
     for ii in np.arange(1, samp_num):
-        if q_cnt >= queries:
-            break
-
         extension = selection + [ii]
-        (s, u) = schur(K[extension][:, extension])
+        s, u = schur(K[extension][: , extension])
+        # u, s = eng.myschur(matlab.double(K[extension][: , extension].tolist()), nargout=2)
+        # u = np.array(u)
+        # s = np.array(s)
         s = np.diag(s)
-        I = (s > tol)
-        A = np.matmul(u[:, I], np.diag(s[I] ** -0.5))
+        I = np.where(s > tol)[0]
+        A = u[:, I] @ np.diag(s[I] ** -0.5)
 
-        restri = np.matmul(np.matmul(np.diag(Y_train[selection]), K[selection][:, extension]), A)
+        y_select_diag = Y_train[selection] if len(selection) == 1 else np.diag(Y_train[selection, 0])
+        restri = y_select_diag @ K[selection][: , extension] @ A
 
-        co1 = np.matmul(np.linalg.pinv(A), coef[extension])
+        co1 = np.linalg.pinv(A) @ coef[extension]
 
         # try:
         co2 = hit_n_run(co1, restri, T)
-        co1 = hit_n_run(co2, restri, T)
-        # except:
-        #     print("Noooo")
+        co1 = hit_n_run(co1, restri, T)
 
-        pred1 = np.matmul(K[ii][extension], np.matmul(A, co1))
-        pred2 = np.matmul(K[ii][extension], np.matmul(A, co2))
+        pred1 = K[ii, extension] @ (A @ co1)
+        pred2 = K[ii, extension] @ (A @ co2)
 
         if (pred1 * pred2 <= 0): # the classifiers disagree
             selection = extension
-            q_cnt += 1
-        if (Y_train[ii] * pred1 >= 0):
-            coef[extension] = np.matmul(A, co1)
-        else:
-            coef[extension] = np.matmul(A, co2)
+            if (Y_train[ii] * pred1 >= 0):
+                coef[extension] = A @ co1
+            else:
+                coef[extension] = A @ co2
 
-        coefs = np.append(coefs, coef)
-        errors.append(np.sum(np.multiply(Y_train, np.matmul(K, coef).squeeze()) <= 0) / len(Y_train))
-        # print((ii, len(selection), errors))
+            coefs.append(copy.deepcopy(coef))
+            errors.append(np.sum(np.multiply(Y_train, K @ coef) <= 0) / len(Y_train))
+            # print((len(selection), errors[-1]))
 
-    return selection, coef, errors
+    return selection, coefs, errors
--- test_synth.py
+++ test_synth.py
@@ -7,21 +7,13 @@ from sklearn import datasets
 import matlab.engine
 import matplotlib.pyplot as plt
 import argparse
-# from mnist.utils import prepare_mnist_data, get_rbf_data
+from mnist.utils import prepare_mnist_data
 from sklearn.metrics.pairwise import rbf_kernel
 import math
+import random
 
-
-def get_rbf_data(lower, upper, num, num_dims):
-    X = np.random.uniform(lower, upper, size=(num,num_dims))
-    Y = []
-    for x1,x2 in X:
-        if x2 < np.sin(10*x1)/5 + 0.3 or ((x2 - 0.8)**2 + (x1 - 0.5)**2) < 0.15**2:
-            Y.append(1)
-        else:
-            Y.append(-1)
-    return X, np.asarray(Y)
-
+# random.seed(1234)
+# np.random.seed(1234)
 
 
 def parse_args():
@@ -43,7 +35,8 @@ def parse_args():
                       default=200, type=int)
   parser.add_argument('--samples', dest='samples',
                       help='List of the samples to run the svm and the kqbc',
-                      default=[5, 8, 10, 13, 18, 20, 23, 28, 30, 33, 38,  40], #, 50, 60, 70, 80, 90, 100, 110, 120, 130],
+                      # default=[5, 8, 10, 13, 18, 20, 23, 28, 30, 33, 38,  40],
+                      default=[5, 10, 20, 30, 40,  50, 60, 70, 80, 90, 100, 110, 120],
                       nargs='+', type=int)
   parser.add_argument('--plot', dest='plot',
                       help='Whether to plot the error comparison',
@@ -51,6 +44,9 @@ def parse_args():
   parser.add_argument('--gamma', dest='gamma',
                       help='rbf gamma value',
                       default=3.0, type=float)
+  parser.add_argument('--param_c', dest='param_c',
+                      help='rbf C value',
+                      default=1.0, type=float)
 
   args = parser.parse_args()
   return args
@@ -67,20 +63,38 @@ if __name__ == '__main__':
     T = args.hnr_iter
     param_dict = {args.dim: args.samples}
 
-    err_dict_svm = {}
-    err_dict_kqbc = {}
-    err_dict_svm_kqbc = {}
-    eng = matlab.engine.start_matlab()
+    err_dict_svm = np.zeros((len(args.samples), args.steps))
+    err_dict_kqbc = np.zeros((len(args.samples), args.steps))
+    err_dict_svm_kqbc = np.zeros((len(args.samples), args.steps))
+    # eng = matlab.engine.start_matlab()
 
-    # creat data
+    # create data
     if args.kernel == 'Linear':
         mu = [0] * args.dim
         cov = np.eye(args.dim)
-        X = np.random.multivariate_normal(mu, cov, size=5000)
+        X = np.random.multivariate_normal(mu, cov, size=10000)
         y = np.sign(X[:, 0])
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     elif args.kernel == 'Gauss':
-        X, y = get_rbf_data(0, 1, 10000, 2)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        X_train, y_train, X_test, y_test = prepare_mnist_data()
+        X_train = X_train / 255.0
+        X_test = X_test / 255.0
+
+
+    param1, param2 = (0, 0) if args.kernel == 'Linear' else (args.gamma, 0.0)
+
+    if args.kernel == 'Linear':
+        K = X_train @ X_train.T
+    elif args.kernel == 'Poly':
+        K = (X_train @ X_train.T + param1) ** param2
+    elif args.kernel == 'Gauss':
+        K = rbf_kernel(X_train, X_train, gamma=param1)
+        # nrm = np.sum(X_train ** 2, 1)
+        # K = np.exp(-(np.tile(nrm, (1, len(Y_train))) - 2 * np.matmul(X_train, X_train.T) +
+        #              np.tile(nrm.T, (len(Y_train), 1))) / (2 * kwargs['parameter1'] ** 2))
+    else:
+        print('Unknown kernel')
+
 
     # XX, y = gen_linear_sep_data(n_samples=10000, n_features=d)
     # X = np.ones((XX.shape[0], XX.shape[1]+1))
@@ -91,69 +105,64 @@ if __name__ == '__main__':
 
     for t in range(args.steps):
         print("Step: %d - running SVM and KQBC" % (t))
-        for d, kl in param_dict.items():
-            if d not in err_dict_svm.keys():
-                err_dict_svm[d] = []
-                err_dict_kqbc[d] = []
-                err_dict_svm_kqbc[d] = []
 
-                param1, param2 = (0, 0) if args.kernel == 'Linear' else (math.sqrt(1/(2*args.gamma)), 0.0)
-                kqbc_selection, coefs = eng.KQBC(matlab.double(X_train.tolist()),
-                                                    matlab.double(np.expand_dims(y_train, 1).tolist()), T,
-                                                    args.kernel, param1, param2, nargout=2)
-                # kqbc_selection, coefs, _ = KQBC(X_train, np.expand_dims(y_train, 1), T, 'Linear')
-                kqbc_selection = np.array(kqbc_selection, dtype=np.int32).squeeze()
-                kqbc_selection -= 1
+        # kqbc_selection, coefs = eng.KQBC(matlab.double(X_train.tolist()),
+        #                                     matlab.double(np.expand_dims(y_train, 1).tolist()), T,
+        #                                     args.kernel, param1, param2, nargout=2)
+        kqbc_selection, coefs, _ = KQBC(K, y_train[:, np.newaxis], T, args.kernel, eng)
+        # kqbc_selection = np.array(kqbc_selection, dtype=np.int32).squeeze()
+        # kqbc_selection -= 1
 
-            for idx, k in enumerate(kl):
-                print("K = %d" % (k))
-                # SVM classifier
-                svm_kernel_name = 'linear' if args.kernel == 'Linear' else 'rbf'
-                svclassifier = SVC(kernel=svm_kernel_name, gamma=args.gamma)
-                # svclassifier = SVC(kernel=svm_kernel_name)
-                if np.abs(np.sum(y_train[:k])) == k:
-                    svclassifier.fit(X_train[k:k+k,:], y_train[k:k+k])
-                else:
-                    svclassifier.fit(X_train[:k,:], y_train[:k])
-                y_pred = svclassifier.predict(X_test)
-                svm_err = np.sum(y_pred * y_test <= 0)/len(y_test)*100
-                if t == 0:
-                    err_dict_svm[d].append(svm_err)
-                else:
-                    err_dict_svm[d][idx] += svm_err
+        for idx, k in enumerate(param_dict[args.dim]):
+            print("K = %d" % (k))
+            # SVM classifier
+            svm_kernel_name = 'linear' if args.kernel == 'Linear' else 'rbf'
+            svclassifier = SVC(kernel=svm_kernel_name, gamma=args.gamma if args.kernel == 'Gauss' else 'auto',
+                               C=args.param_c)
+            # svclassifier = SVC(kernel=svm_kernel_name)
 
-                # Run KQBC matlab code
-                coef = np.array(coefs, dtype=np.float32)[:, k-1]
-                # K = rbf_kernel(X_train, X_train, gamma=param1)
-                # kqbc_cls = X_train.T @ coef if args.kernel == 'Linear' else K @ coef
+            svm_idx = random.sample(range(0, y_train.shape[0]), k)
+            while np.sum(y_train[svm_idx]) in [k, -k]:
+                svm_idx = random.sample(range(0, y_train.shape[0]), k)
+            svclassifier.fit(X_train[svm_idx,:], y_train[svm_idx])
+            y_pred = svclassifier.predict(X_test)
+            svm_err = np.sum(y_pred * y_test <= 0)/len(y_test)*100
+            err_dict_svm[k, t] = svm_err
+
+            # coef = np.array(coefs, dtype=np.float32)[:, k-1]
+            # K = rbf_kernel(X_train, X_train, gamma=param1)
 
 
-                # SVM with KQBC selections
-                # kqbc_svm = SVC(kernel='linear')
-                # kqbc_svm.fit(X_train[kqbc_selection[:k],:], y_train[kqbc_selection[:k]])
-                # y_pred_svm_kqbc = kqbc_svm.predict(X_test)
-                # kqbc_svm_err = np.sum(y_pred_svm_kqbc * y_test <= 0) / len(y_test) * 100
-                # if t == 0:
-                #     err_dict_svm_kqbc[d].append(kqbc_svm_err)
-                # else:
-                #     err_dict_svm_kqbc[d][idx] += kqbc_svm_err
+
+            # SVM with KQBC selections
+            # kqbc_svm = SVC(kernel='linear')
+            # kqbc_svm.fit(X_train[kqbc_selection[:k],:], y_train[kqbc_selection[:k]])
+            # y_pred_svm_kqbc = kqbc_svm.predict(X_test)
+            # kqbc_svm_err = np.sum(y_pred_svm_kqbc * y_test <= 0) / len(y_test) * 100
+            # if t == 0:
+            #     err_dict_svm_kqbc[d].append(kqbc_svm_err)
+            # else:
+            #     err_dict_svm_kqbc[d][idx] += kqbc_svm_err
 
 
-                # Dot prod with KQBC classifier
+            # Dot prod with KQBC classifier
+            coef = coefs[k - 1]
+            if args.kernel == 'Linear':
+                kqbc_cls = X_train.T @ coef
+                y_pred_kqbc = np.sign(X_test @ kqbc_cls)
+            elif args.kernel == 'Gauss':
                 y_pred_kqbc = np.sign(rbf_kernel(X_test, X_train, gamma=args.gamma) @ coef)
-                # y_pred_kqbc = np.sign(X_test @ kqbc_cls)
-                # if np.sum(y_pred_kqbc != y_test) > np.sum(y_pred_kqbc == y_test):
-                #     y_pred_kqbc = -y_pred_kqbc
-                kqbc_err = np.sum(y_test * y_pred_kqbc <= 0)/len(y_test)*100
-                print("KQBC error = %.2f \t SVM error = %.2f" % (kqbc_err, svm_err))
-                if t == 0:
-                    err_dict_kqbc[d].append(kqbc_err)
-                else:
-                    err_dict_kqbc[d][idx] += kqbc_err
+
+            kqbc_err = np.sum(y_test * y_pred_kqbc.squeeze() <= 0)/len(y_test)*100
+            print("KQBC error = %.2f \t SVM error = %.2f" % (kqbc_err, svm_err))
+            err_dict_kqbc[k ,t] = kqbc_err
+
+
 
 
     if args.plot:
         df = pd.DataFrame({'x': args.samples,
-                           'SVM': np.array([i / args.steps for i in err_dict_svm[args.dim]]),
-                           'KQBC': np.array([i / args.steps for i in err_dict_kqbc[args.dim]])})
-        df.plot(x='x', logy=True, ylim=(0.1, 1e2), title='Generalization error: SVM vs. KQBC (data dim = %d)' % (args.dim))
+                           'SVM': err_dict_svm.mean(axis=1),
+                           'KQBC': err_dict_kqbc.mean(axis=1)})
+        df.plot(x='x', logy=True, ylim=(0.1, 1e2), yerr=[err_dict_svm.std(axis=1), err_dict_kqbc.std(axis=1)],
+                                                         title='Generalization error: SVM vs. KQBC (data dim = %d)' % (args.dim))
